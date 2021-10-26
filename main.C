ac817058423777bfd73d77ccd412648b3b3150d4
Gene Cook <genec_1@earthlink.net>
2021-10-26 12:06:11 -0600

    add comments to main

--- main.C
+++ main.C
@@ -7,11 +7,18 @@
 
 #define FLAG_VALUE 123
 
+//********************************************************************
+// entry point, core 1...
+//********************************************************************
+
 void core1_main() {
-  // let other core know this core is active, verify
-  // other core as well...
+  printf("Core 1 active...\n");
+  
+  // let other core know this core is active...
   
   multicore_fifo_push_blocking(FLAG_VALUE);
+
+  // now wait for other cores response...
   
   uint32_t g = multicore_fifo_pop_blocking();
   if (g == FLAG_VALUE) {
@@ -20,7 +27,7 @@ void core1_main() {
     printf("MULTICORE PROBLEMS DETECTED - CORE 1 ???\n");
   }
 
-  printf("Core 1 active...\n");
+  printf("Core 0 active, detected from core 1...\n");
   
   // both cores active and okay to proceed...
 
@@ -31,29 +38,35 @@ void core1_main() {
   
 }
 
+//********************************************************************
+// (main) entry point - core 0...
+//********************************************************************
+
 int main() {
   stdio_init_all();
-  printf("Starting...\n");
+  printf("Starting, core 0 active...\n");
 
-  // launch other core, verify its active...
+  // launch other core, wait for it to respond...
   
   multicore_launch_core1(core1_main);
-  
   uint32_t g = multicore_fifo_pop_blocking();
+
+  // now send other core this cores 'response'...
+  
   if (g == FLAG_VALUE) {
     multicore_fifo_push_blocking(FLAG_VALUE);
   } else {
     printf("MULTICORE PROBLEMS DETECTED - CORE 0 ???\n");
   }
   
-  printf("Core 0 active...\n");
+  printf("Core 1 active, detected from core 0...\n");
   
   // both cores active and okay to proceed...
   
   while(true) {
-    printf("Hello Dude!\n");
+    printf("Dude, dude, dude!\n");
     int c = getchar();
-    multicore_fifo_push_blocking(c);
+    multicore_fifo_push_blocking(c); 
     sleep_ms(1000);
   }
   
