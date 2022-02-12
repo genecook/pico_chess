b63360ecbc53568a13fb6dc8db3af84726412751
Gene Cook <genec@genes-cube>
2022-02-12 06:13:16 -0600

    bug fix: evaluate queens-side castling independent of kings-side castling

--- src/moves_tree.C
+++ src/moves_tree.C
@@ -64,12 +64,8 @@ bool MovesTree::GetMoves(std::vector<Move> *possible_moves, Board &game_board, i
       // validate castling, kings side...
       squares_to_check[0] = 5;
       squares_to_check[1] = 6;
-    } else if (game_board.CastleValid(color,/* queen-side */ false)) {
-    // validate castling, queens side...
-      squares_to_check[0] = 1;
-      squares_to_check[1] = 2;
-      squares_to_check[2] = 3;
     }
+    
     if (squares_to_check[0] != 0)
       for (int i = 0; (i < 8) && castling_enabled; i++) {
          for (int j = 0; (j < 8) && castling_enabled; j++) {
@@ -85,6 +81,30 @@ bool MovesTree::GetMoves(std::vector<Move> *possible_moves, Board &game_board, i
 	    }
          }
       }
+
+    if (game_board.CastleValid(color,/* queen-side */ false)) {
+    // validate castling, queens side...
+      squares_to_check[0] = 1;
+      squares_to_check[1] = 2;
+      squares_to_check[2] = 3;
+    }
+    
+    if (squares_to_check[0] != 0)
+      for (int i = 0; (i < 8) && castling_enabled; i++) {
+         for (int j = 0; (j < 8) && castling_enabled; j++) {
+            int piece_type, piece_color;
+            if (game_board.GetPiece(piece_type, piece_color,i,j)) {
+	      if (piece_color == color)
+	        continue; // this is 'our' piece...
+	      // for current board state, does 'opponents' piece 'cover' a 'castling' square?
+	      for (int k = 0; (squares_to_check[k] != 0) && castling_enabled; k++)
+	         if (pieces.Check(game_board,kings_row,squares_to_check[k],piece_type,piece_color,i,j)) {
+	           castling_enabled = false;
+	      }
+	    }
+         }
+      }
+    
   }
     
   std::vector<Move> all_possible_moves;
